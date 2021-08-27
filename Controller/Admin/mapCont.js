77d7d18959945f05ded686c827ede04b7866f641
Jueun <zxnm0901@naver.com>
2021-08-27 10:56:50 +0900

    classToFunction: MapCont

--- Controller/Admin/mapCont.js
+++ Controller/Admin/mapCont.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
 import Map from '../../View/Admin/mapView';
 import { Entypo } from '@expo/vector-icons';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import ipCode from './ipcode';
-export default class MapCont extends Component {
-	state = {
-		lat : 0,
-		lon : 0
-	};
-	onRefresh = () => {
-		this.setState({ refreshing: true });
+
+const MapCont = ({ navigation }) => {
+	const [lat, setLat] = useState(0);
+	const [lon, setLon] = useState(0);
+	const [refreshing, setRefreshing] = useState(false);
+    
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+
+	const onRefresh = () => {
+		setRefreshing(true);
+		
 		setTimeout(() => {
-			this.getData().then(() => {
-				this.setState({ refreshing: false });
+			fetchData().then(() => {
+				setRefreshing(false)
 			});
-		}, 1000);
+		}, 1000)
 	};
-	getData = async () => {
+
+	const fetchData = async () => {
 		var ip = ipCode();
 		const { data } = await axios.get(`http://${ip}:3001/`);
-
-		this.setState({ lat: data[0].lat, lon: data[1].lon });
-		var hour = new Date().getHours();
-		var min = new Date().getMinutes();
-		var sec = new Date().getSeconds();
-		var mil = new Date().getMilliseconds();
-
-		console.log('끝', hour + '시' + min + '분' + sec + '.' + mil + '초');
+		setLat(data[0].lat);
+		setLon(data[1].lon);
+		
 	};
-
-	setHeaderOptions(navigation) {
+	const setHeaderOptions = (navigation) => {
 		navigation.setOptions({
-			headerLeft  : () => (
+			headerLeft: () => (
 				<Entypo
 					name="cw"
 					size={40}
@@ -41,7 +41,7 @@ export default class MapCont extends Component {
 					style={{ paddingLeft: 20 }}
 				/>
 			),
-			headerRight : () => (
+			headerRight: () => (
 				<Entypo
 					name="arrow-bold-left"
 					size={40}
@@ -51,17 +51,10 @@ export default class MapCont extends Component {
 				/>
 			)
 		});
-	}
-	componentDidMount() {
-		this.getData();
-	}
-	render() {
-		var hour = new Date().getHours();
-		var min = new Date().getMinutes();
-		var sec = new Date().getSeconds();
-		var mil = new Date().getMilliseconds();
-		console.log('시작', hour + '시' + min + '분' + sec + '.' + mil + '초');
-		const { lat, lon } = this.state;
-		return this.setHeaderOptions(this.props.navigation), <Map lat={lat} lon={lon} refresh={this.onRefresh} />;
-	}
-}
+	};
+	return (setHeaderOptions(navigation), < Map lat = { lat } lon = { lon } refresh = {onRefresh } />);
+
+};
+
+export default MapCont;
+
