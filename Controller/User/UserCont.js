a4a497a3d03e9b437caf26c10afb6fb2e53335a3
Jueun <zxnm0901@naver.com>
2021-08-29 12:30:51 +0900

    lassToFunction: UserCont

--- Controller/User/UserCont.js
+++ Controller/User/UserCont.js
@@ -1,41 +1,25 @@
-import React from 'react';
+import React,{ useState,useEffect } from 'react';
 import axios from 'axios';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import ipCode from '../Admin/ipcode';
 import UserView from '../../View/User/UserView';
-class User extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			info  : '',
-			desti : ''
-		};
-	}
-	FetchData = async () => {
+
+const User = (props) => {
+	const [info, setInfo] = useState('');
+
+	useEffect(() => {
+		fetchData();
+	})
+
+	const fetchData = async () => {
 		var ip = ipCode();
 		const { data } = await axios.get(`http://${ip}:3000/Delivery`);
-		this.setState({ info: data });
+		setInfo(data);
 	};
 
-	componentDidMount() {
-		this.FetchData();
-		this._unsubscribe = this.props.navigation.addListener('focus', () => {
-			this.onRefresh();
-		});
-	}
-	onRefresh = () => {
-		this.setState({ refreshing: true });
-		this.FetchData().then(() => {
-			this.setState({ refreshing: false });
-		});
-	};
-	componentWillUnmount() {
-		this._unsubscribe();
-	}
-	render() {
-		return (
-			<UserView info={this.state.info} navigation={this.props.navigation} />
-		);
-	}
-}
+	return (
+		<UserView info={info} navigation={props.navigation} />
+	);
+
+};
+
 export default User;
