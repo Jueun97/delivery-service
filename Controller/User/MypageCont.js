e806d79f51773ee3017966958d886a9b634f5857
Jueun <zxnm0901@naver.com>
2021-08-29 12:18:24 +0900

    classToFunction: MypageCont

--- Controller/User/MypageCont.js
+++ Controller/User/MypageCont.js
@@ -1,42 +1,26 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
-import { Entypo, FontAwesome } from '@expo/vector-icons';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import ipCode from '../Admin/ipcode';
 import MypageView from '../../View/User/MypageView';
 
-export default class Mypage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			info : ''
-		};
-	}
-	FetchData = async () => {
+const Mypage = (props) => {
+	const [info, setInfo] = useState('');
+	const { adminKey } = props.route.params;
+
+	useEffect(() => {
+		fetchData();
+	});
+
+	fetchData = async () => {
 		var ip = ipCode();
 		const { data } = await axios.get(`http://${ip}:3000/User`);
-		this.setState({ info: data });
+		setInfo(data);
 	};
-	onRefresh = () => {
-		this.setState({ refreshing: true });
-		this.FetchData().then(() => {
-			this.setState({ refreshing: false });
-		});
-	};
-	componentDidMount() {
-		this.FetchData();
-		this._unsubscribe = this.props.navigation.addListener('focus', () => {
-			this.onRefresh();
-		});
-	}
-	componentWillUnmount() {
-		this._unsubscribe();
-	}
-	render() {
-		const { info } = this.state;
-		const { adminKey } = this.props.route.params;
-		return (
-			<MypageView info={info} adminKey={adminKey} navigation={this.props.navigation} />
-		);
-	}
-}
+
+	return (
+		<MypageView info={info} adminKey={adminKey} navigation={props.navigation} />
+	)
+};
+
+export default Mypage;
+
