0d5036fe0280fc1dde81096917d07af3b1612e56
Jueun <zxnm0901@naver.com>
2021-08-29 12:41:02 +0900

    classToFunction:useDetailsCont

--- Controller/User/userDetailsCont.js
+++ Controller/User/userDetailsCont.js
@@ -1,48 +1,54 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Image, Text, ImageBackground, Alert } from 'react-native';
-import { TextInput, TouchableHighlight, ScrollView } from 'react-native-gesture-handler';
-import { Entypo, FontAwesome } from '@expo/vector-icons';
+import React, { useEffect,useState } from 'react';
+import { Alert } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import axios from 'axios';
 import ipCode from '../Admin/ipcode';
 import DetailsView from '../../View/User/userDetailsView';
 
-export default class Detail extends Component {
-	state = { time: '', disable: false };
-	getData = async (building) => {
+const Detail = (props) => {
+	const [time, setTime] = useState('');
+	const [disable, setDisable] = useState(false);
+	const {list} = props.route.params;
+
+	useEffect(() => {
+		fetchData(list.건물명);
+	}, [fetchData]);
+
+	const fetchData = async (building) => {
 		var ip = ipCode();
 		const { data } = await axios.get(`http://${ip}:3000/delivery`);
 		for (var i = 0; i < data.length; i++) {
-			if (data[i].건물명 == building) this.setState({ time: data[i].배송시간 });
+			if (data[i].건물명 == building)
+				setTime(data[i].배송시간)
 		}
-		this.checkTime();
+		checkTime();
 	};
-	checkTime() {
-		var { time } = this.state;
+	const checkTime = () => {
+		var { time } = state;
 		var hours = new Date().getHours();
 		var min = new Date().getMinutes();
 		var hoursU = parseInt(time[0]) * 10 + parseInt(time[1]);
 		var minU = parseInt(time[3]) * 10 + parseInt(time[4]);
 		if (minU == 0) {
 			if (hours >= hoursU) {
-				this.setState({ disable: true });
+				setDisable(true);
 			} else if (hours == hoursU - 1 && min >= 50) {
-				this.setState({ disable: true });
+				setDisable(true);
 			}
 		} else {
 			if (hours > hoursU || (hours == hoursU && min >= 20)) {
-				this.setState({ disable: true });
+				setDisable(true);
 			}
 		}
 	}
-	setHeaderOptions(navigation) {
-		const { list } = this.props.route.params;
+	const setHeaderOptions = (navigation) => {
 		var user = list.주문자번호;
 		navigation.setOptions({
 			title       : user,
 			headerLeft  : () => (
 				<Entypo
 					name="home"
-					si	ze={40}
+					size={40}
 					color={'black'}
 					onPress={() => navigation.navigate('home')}
 					style={{ paddingLeft: 20 }}
@@ -60,8 +66,7 @@ export default class Detail extends Component {
 		});
 	}
 
-	handleDelete(UserID, doc, building, navigation) {
-		var check = 0;
+	const handleDelete = (UserID, doc, building, navigation) => {
 		var ip = ipCode();
 
 		Alert.alert('에약취소', '진행하시겠습니끼?', [
@@ -93,23 +98,18 @@ export default class Detail extends Component {
 			}
 		]);
 	}
-	componentDidMount() {
-		const { list } = this.props.route.params;
-		this.getData(list.건물명);
-	}
-	render() {
-		const { list } = this.props.route.params;
-		return (
-			this.setHeaderOptions(this.props.navigation),
-			(
-				<DetailsView
-					list={list}
-					time={this.state.time}
-					disable={this.state.disable}
-					navigation={this.props.navigation}
-					handleDelete={this.handleDelete}
-				/>
-			)
-		);
-	}
-}
+	return (
+		setHeaderOptions(props.navigation),
+		(
+			<DetailsView
+				list={list}
+				time={time}
+				disable={disable}
+				navigation={props.navigation}
+				handleDelete={handleDelete}
+			/>
+		)
+	);
+};
+
+export default Detail;
